d7c9f0aa6695678e072cdac00a62ed9bba6a993e
gearwheels <70712596+gearwheels@users.noreply.github.com>
2022-10-22 15:35:16 +0300

    исправил варик 1

--- main.py
+++ main.py
@@ -906,35 +906,10 @@ class Window6(QMainWindow):
         quit.triggered.connect(self.closeEvent)
 
 
-        self.timer = QTimer()
-        self.timer.setInterval(100)
-        self.timer.timeout.connect(self.update_plot)
-        self.timer.start()
-
-
-    def update_plot(self):
-        # Drop off the first y element, append a new one.
-        #self._canvas.axes.cla()  # Clear the canvas.
-        # Trigger the canvas to update and redraw.
-        AdjacencyMatrixPeople = np.zeros((len(graph1.AdjacencyMatrix), len(graph1.AdjacencyMatrix)))
-        for i in range(len(AdjacencyMatrixPeople)):
-            for j in range(len(AdjacencyMatrixPeople[i])):
-                if graph1.AdjacencyMatrix[i][j] != 0:
-                    AdjacencyMatrixPeople = graph5[0].label[i][j].text()
-        X_max = 1000
-        X_min = 0
-        step = 75
-        intervals = np.zeros(int((X_max-X_min)/step))
-        for i in range(len(AdjacencyMatrixPeople)):
-            for j in range(len(AdjacencyMatrixPeople[i])):
-                if AdjacencyMatrixPeople[i][j] != 0:
-                    for k in range(len(intervals)):
-                        if k*step <= graph1.Points[i][0] and (k+1)*step >= graph1.Points[j][0]:
-                            intervals[k] += AdjacencyMatrixPeople[i][j]
-        # print(intervals)
-        #self._canvas.axes.cla()  # Clear the canvas.
-
-        #self._canvas.draw()
+        # self.timer = QTimer()
+        # self.timer.setInterval(100)
+        # self.timer.timeout.connect(self.update_plot)
+        # self.timer.start()
 
 
     def closeEvent(self, event):
