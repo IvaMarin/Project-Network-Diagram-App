32184c7bc417b9630ea3ea3520cc27de7dca7e7b
gearwheels <70712596+gearwheels@users.noreply.github.com>
2022-10-22 10:37:11 +0300

    Update initial_encrypt.py

--- initial_encrypt.py
+++ initial_encrypt.py
@@ -1,13 +1,4 @@
 from pathlib import Path
-<<<<<<< Updated upstream
 import encrypt_module
 
 encrypt_module.encrypt_files(Path("encrypted_data"), "pirat_encrypt123")
-=======
-from utils.encrypt import encrypt_module
-
-encrypt_module.encrypt_files(Path("../../encrypted_data"), "pirat_encrypt123")
->>>>>>> Stashed changes
-
-# with open("res1.docx", "wb") as f:
-#     f.write(encrypt_module.decrypt_file("../../reports", "f_f.docx", "pirat_encrypt123"))
