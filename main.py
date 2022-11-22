9a8917281b41efc739a068cb53e2f9b6050c5b23
weiterSchlafen <frog070401@mail.ru>
2022-11-22 12:13:25 +0300

    баг в пятом

--- main.py
+++ main.py
@@ -1207,6 +1207,8 @@ class Window5(QMainWindow):
                 for row in range(properties.n):
                     self.item = QtWidgets.QTableWidgetItem(str(properties.tp[row]))
                     self.table.ui.tableWidget.setItem(row, 2, self.item)
+                for i in self.widgetList:
+                    i.functionAble = ""
                 self.table.resize(500, 700)
             self.checkForm = task5CheckForm(self, mistakes, 1)
             self.checkForm.exec_()
@@ -1245,7 +1247,9 @@ class Window5(QMainWindow):
                 self.table.ui.tableWidget.setHorizontalHeaderLabels(["Шифр", "Кол-во людей"])
                 for row in range(MainWindow.ui.tableVar.rowCount()):
                     self.item = QtWidgets.QTableWidgetItem(MainWindow.ui.tableVar.item(row, 2).text())
-                    self.table.ui.tableWidget.setItem(row, 1, self.item) 
+                    self.table.ui.tableWidget.setItem(row, 1, self.item)
+                for i in self.widgetList:
+                    i.functionAble = ""
                 self.table.resize(393, 700)
             self.checkForm = task5CheckForm(self, mistakes, 2)
             self.checkForm.exec_()
@@ -1269,6 +1273,8 @@ class Window5(QMainWindow):
             if is_correct:
                 statusTask.set__verification_passed_task(5)
                 self.ui.actionbtnCheck.setVisible(False)
+                for i in self.widgetList:
+                    i.functionAble = ""
             self.checkForm = task5CheckForm(self, mistakes, 3)
             self.checkForm.exec_()
 
