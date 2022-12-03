dd0454f412e7d3343d367eef33fa53fb2b7b9673
gearwheels <70712596+gearwheels@users.noreply.github.com>
2022-12-03 14:32:37 +0300

    hotfix списка

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 from pathlib import Path
 
 from PyQt5 import QtWidgets, QtCore
-import win32com.client
+
 from PyQt5.QtCore import QRect, Qt, QSize
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QMessageBox, QAction, QDialog, QLineEdit
 from PyQt5.QtGui import QPixmap, QScreen, QImage
@@ -17,9 +17,6 @@ from PIL import Image
 from fpdf import FPDF
 from docx import Document
 from docx.shared import Inches
-import docx2txt
-import PyPDF2
-
 
 # from borb.pdf import Document
 # from borb.pdf import Page
@@ -266,7 +263,7 @@ class Window1(QMainWindow):
         if properties.teacherMode:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(255,0,0,255)}")
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(255,0,0,255)}")
-            property.enter_teacher_mode[1] = True
+            property.enter_teacher_mode[0] = True
         else:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(184, 255, 192,255)}")  #rgb(184, 255, 192)
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(184, 255, 192,255)}")
@@ -429,7 +426,7 @@ class Window2(QMainWindow):
         if properties.teacherMode:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(255,0,0,255)}")
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(255,0,0,255)}")
-            property.enter_teacher_mode[2] = True
+            property.enter_teacher_mode[1] = True
         else:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(184, 255, 192,255)}")  #rgb(184, 255, 192)
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(184, 255, 192,255)}")
@@ -739,7 +736,7 @@ class Window3(QMainWindow):
         if properties.teacherMode:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(255,0,0,255)}")
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(255,0,0,255)}")
-            property.enter_teacher_mode[3] = True
+            property.enter_teacher_mode[2] = True
         else:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(184, 255, 192,255)}")  #rgb(184, 255, 192)
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(184, 255, 192,255)}")
@@ -927,7 +924,7 @@ class Window4(QMainWindow):
         if properties.teacherMode:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(255,0,0,255)}")
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(255,0,0,255)}")
-            property.enter_teacher_mode[4] = True
+            property.enter_teacher_mode[3] = True
         else:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(184, 255, 192,255)}")  #rgb(184, 255, 192)
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(184, 255, 192,255)}")
@@ -1362,7 +1359,7 @@ class Window5(QMainWindow):
         if properties.teacherMode:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(255,0,0,255)}")
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(255,0,0,255)}")
-            property.enter_teacher_mode[5] = True
+            property.enter_teacher_mode[4] = True
         else:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(184, 255, 192,255)}")  #rgb(184, 255, 192)
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(184, 255, 192,255)}")
@@ -1591,7 +1588,7 @@ class Window6(QMainWindow):
         if properties.teacherMode:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(255,0,0,255)}")
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(255,0,0,255)}")
-            property.enter_teacher_mode[6] = True
+            property.enter_teacher_mode[5] = True
         else:
             self.ui.menuBar.setStyleSheet("QMenuBar{background:rgba(184, 255, 192,255)}")  #rgb(184, 255, 192)
             self.ui.statusbar.setStyleSheet("QStatusBar{background:rgba(184, 255, 192,255)}")
@@ -1789,7 +1786,6 @@ class WindowMenu(QMainWindow):
         self.ui.btnReportSign.clicked.connect(self.winSigReport.exec) # по клику вызываем диалоговое окно для подписти отчета и передаем управление ему
         self.ui.btnGenVar.clicked.connect(lambda: self.testGen()) # по клику генерируем задание (заполняем таблицу)
         self.ui.previewReport.clicked.connect(lambda: self.creatReport()) #
-        self.ui.btnPrint.clicked.connect(lambda: self.printReport())
         self.ui.btnEditTaskVariant.clicked.connect(self.winEditTable.exec)
 
 
@@ -1877,11 +1873,6 @@ class WindowMenu(QMainWindow):
         document.add_page_break()
 
         document.save('Отчет по лаборатрной работе.docx')
-
-    def printReport(self):
-        # extract text
-        result = docx2txt.process("Отчет по лаборатрной работе.docx","")
-        print(result)
 
     def openTask (self, numTask):
         if not(self.ui.btnTeacherMode.isChecked()):
