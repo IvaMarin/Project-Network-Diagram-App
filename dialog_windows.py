d3dcd98156f2160fdc20e27691b5b706da581e6d
gearwheels <70712596+gearwheels@users.noreply.github.com>
2023-03-11 13:30:43 +0300

    исправления дизайна
    добавил линию
    поменял ориентацию кнопок в редакторе таблиц

--- dialog_windows.py
+++ dialog_windows.py
@@ -57,7 +57,6 @@ class winSigReport(QtWidgets.QDialog): # окно изменения личны�
                                 | QtCore.Qt.CustomizeWindowHint 
                                 | QtCore.Qt.WindowCloseButtonHint)
 
-        self.ui.lineEditNumINGroup.setValidator(QIntValidator())
 
         rx = QtCore.QRegExp("[a-zA-Zа-яА-Я .,]{200}")
         val = QtGui.QRegExpValidator(rx)
@@ -72,7 +71,7 @@ class winSigReport(QtWidgets.QDialog): # окно изменения личны�
         self.move(int(sizeWindow.width() / 20), int(sizeWindow.height() / 20)) # двигаем окно левее и выше
 
         self.ui.lineEditSurname.insert(self.mainMenu.surname)       # подгружаем из mainMenu данные если они уже были указаны
-        self.ui.lineEditNumINGroup.insert(self.mainMenu.numINGroup) #
+        # self.ui.lineEditNumINGroup.insert(self.mainMenu.numINGroup) #
         self.ui.lineEditGroup.insert(self.mainMenu.numGroup)        #
 
         self._connectAction() # ф-ия связи с эл-тами окна
--- qt_designer_ui/winEditTable.py
+++ qt_designer_ui/winEditTable.py
@@ -65,6 +65,13 @@ class Ui_CreatEditTask(object):
         self.horizontalLayout.setStretch(2, 10)
         self.horizontalLayout.setStretch(3, 11)
         self.verticalLayout.addLayout(self.horizontalLayout)
+        self.line = QtWidgets.QFrame(CreatEditTask)
+        self.line.setLineWidth(30)
+        self.line.setMidLineWidth(30)
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.verticalLayout.addWidget(self.line)
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setContentsMargins(10, 10, 10, 130)
         self.gridLayout.setHorizontalSpacing(10)
@@ -113,10 +120,20 @@ class Ui_CreatEditTask(object):
 
     def retranslateUi(self, CreatEditTask):
         _translate = QtCore.QCoreApplication.translate
-        CreatEditTask.setWindowTitle(_translate("CreatEditTask", "Редактор таблиц вариантов"))
-        self.labelHelpLeft.setText(_translate("CreatEditTask", "Нумирация варианта:"))
+        CreatEditTask.setWindowTitle(_translate("CreatEditTask", "Редактирование таблиц вариантов работ"))
+        self.labelHelpLeft.setText(_translate("CreatEditTask", "Номер варианта:"))
         self.lineEdit.setPlaceholderText(_translate("CreatEditTask", "Введите номер варианта"))
         self.btnCreatTable.setText(_translate("CreatEditTask", "Создать"))
         self.btnEditTable.setText(_translate("CreatEditTask", "Редактировать"))
         self.labelHelp.setText(_translate("CreatEditTask", "Выберите вариант задания, который вы хотите редактировать или удалить."))
         self.btnDeletTable.setText(_translate("CreatEditTask", "Удалить"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    CreatEditTask = QtWidgets.QDialog()
+    ui = Ui_CreatEditTask()
+    ui.setupUi(CreatEditTask)
+    CreatEditTask.show()
+    sys.exit(app.exec_())
